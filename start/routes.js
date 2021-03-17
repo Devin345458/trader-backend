66e4808827e1f2a9e0fefd71a383af475bf54fcf
Devin <devin@marketmakerleads.com>
2021-03-17 11:31:04 -0500

    Fix document root?

--- start/routes.js
+++ start/routes.js
@@ -23,43 +23,43 @@ Route.get('/', () => {
 /**
  * User Routes
  */
-Route.post('/v1/users', 'V1/UserController.add')
-Route.patch('/v1/users', 'V1/UserController.edit')
-Route.post('/v1/users/login', 'V1/UserController.login')
-Route.get('/v1/users/logged-in-user', 'V1/UserController.loggedInUser').middleware(['auth'])
+Route.post('/v1/users', 'v1/UserController.add')
+Route.patch('/v1/users', 'v1/UserController.edit')
+Route.post('/v1/users/login', 'v1/UserController.login')
+Route.get('/v1/users/logged-in-user', 'v1/UserController.loggedInUser').middleware(['auth'])
 
 
 /**
  * Coinbase Routes
  */
-Route.get('/v1/coinbase/coins/:paper', 'V1/CoinbaseController.coins').middleware(['auth'])
-Route.post('/v1/coinbase/profiles/:type', 'V1/CoinbaseController.profiles').middleware(['auth'])
+Route.get('/v1/coinbase/coins/:paper', 'v1/CoinbaseController.coins').middleware(['auth'])
+Route.post('/v1/coinbase/profiles/:type', 'v1/CoinbaseController.profiles').middleware(['auth'])
 
 
 /**
  * Profile Routes
  */
-Route.get('/v1/profiles/:type?', 'V1/ProfileController.index').middleware(['auth'])
-Route.get('/v1/profiles/view/:id', 'V1/ProfileController.view').middleware(['auth'])
-Route.post('/v1/profiles', 'V1/ProfileController.add').middleware(['auth']).validator('Profile')
-Route.patch('/v1/profiles', 'V1/ProfileController.edit').middleware(['auth']).validator('Profile')
-Route.delete('/v1/profiles/:id', 'V1/ProfileController.delete').middleware(['auth'])
+Route.get('/v1/profiles/:type?', 'v1/ProfileController.index').middleware(['auth'])
+Route.get('/v1/profiles/view/:id', 'v1/ProfileController.view').middleware(['auth'])
+Route.post('/v1/profiles', 'v1/ProfileController.add').middleware(['auth']).validator('Profile')
+Route.patch('/v1/profiles', 'v1/ProfileController.edit').middleware(['auth']).validator('Profile')
+Route.delete('/v1/profiles/:id', 'v1/ProfileController.delete').middleware(['auth'])
 
 
 /**
  * Strategy Routes
  */
-Route.get('/v1/strategies', 'V1/StrategyController.index').middleware(['auth'])
-Route.get('/v1/strategies/:id', 'V1/StrategyController.view').middleware(['auth'])
-Route.get('/v1/strategies/options/:indicator', 'V1/StrategyController.options').middleware(['auth'])
-Route.post('/v1/strategies', 'V1/StrategyController.add').middleware(['auth']).validator('Strategy')
-Route.patch('/v1/strategies', 'V1/StrategyController.edit').middleware(['auth'])
-Route.delete('/v1/strategies/:id', 'V1/StrategyController.delete').middleware(['auth'])
+Route.get('/v1/strategies', 'v1/StrategyController.index').middleware(['auth'])
+Route.get('/v1/strategies/:id', 'v1/StrategyController.view').middleware(['auth'])
+Route.get('/v1/strategies/options/:indicator', 'v1/StrategyController.options').middleware(['auth'])
+Route.post('/v1/strategies', 'v1/StrategyController.add').middleware(['auth']).validator('Strategy')
+Route.patch('/v1/strategies', 'v1/StrategyController.edit').middleware(['auth'])
+Route.delete('/v1/strategies/:id', 'v1/StrategyController.delete').middleware(['auth'])
 
 
 /**
  * Trade Routes
  */
-Route.get('/v1/trades/strategy/:id', 'V1/TradeController.strategy').middleware(['auth'])
-Route.get('/v1/trades/get-ticks/:id', 'V1/TradeController.getTicks').middleware(['auth'])
-Route.get('/v1/trades/get-indicators/:id', 'V1/TradeController.getIndicators').middleware(['auth'])
+Route.get('/v1/trades/strategy/:id', 'v1/TradeController.strategy').middleware(['auth'])
+Route.get('/v1/trades/get-ticks/:id', 'v1/TradeController.getTicks').middleware(['auth'])
+Route.get('/v1/trades/get-indicators/:id', 'v1/TradeController.getIndicators').middleware(['auth'])
